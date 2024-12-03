bdd3dcfa7f3219faaa69391abb5508504c640b79
Vic Kowa <kowavic@gmail.com>
2024-12-03 12:28:47 +0100

    Revert "Finished getSales for salesman but products are missing"
    This reverts commit d7496226315e844b86d4b1d23e9c078cb0a87786.

--- backend/src/services/open-crx-service.js
+++ backend/src/services/open-crx-service.js
@@ -1,39 +1,45 @@
 const axios = require('axios');
 const {response} = require("express");
-const Sale = require('../models/OpenCRX/Sale.js');
 
 const SalesAPI = "https://sepp-crm.inf.h-brs.de/opencrx-rest-CRX/org.opencrx.kernel.contract1/provider/CRX/segment/Standard";
+
+const username = "guest";
+const password = "guest";
+
+const headers = {
+    'Authorization': 'Basic ' + Buffer.from(`${username}:${password}`).toString('base64'),
+    'Accept': 'application/json',
+}
 
 exports.getAllSales = async function (){
 
 }
 
-
 exports.getSales = async function (sid){
-
+    try {
         // fetch all SalesOrders from OpenCRX
-        const { data } = await axios.get(`${SalesAPI}/salesOrder`, { headers: Sale.headers }).catch(
-            _ => {
-                throw new Error('Error fetching sales from OpenCRX');
-            }
-        );
+        const { data } = await axios.get(`${SalesAPI}/salesOrder`, { headers});
         const salesOrders = data.objects;
 
-        const salesOrders_filtered =(await Promise.all(
+        return (await Promise.all(
             salesOrders
                 .map(async (order) => {
+                    // check if order has href to order
+                    const salesRep = order['salesRep']['@href'];
+                    if (!salesRep) return null;
 
-                    // get salesRep from OpenCRX
-                    const governmentId = await Sale.getSalesRep(order);
-                    return governmentId === sid ? order : null;
+                    const response = await axios.get(salesRep, {headers});
+
+                    const governmentId = response.data['governmentId'];
+                    if (!governmentId) return null;
+
+                    return governmentId.toString() === sid ? order : null;
                 })
-        ).catch(
-            _ => {
-                throw new Error('Error fetching sales from OpenCRX');
-            }
         )).filter(order => order !== null);
 
-        // map salesOrders to Sale objects
-        return await Promise.all(salesOrders_filtered.map(order => Sale.fromJSON(order)));
+    } catch (error) {
+        console.error('Error fetching sales from OpenCRX', error);
+        throw new Error('Error fetching sales from OpenCRX');
+    }
 }
 
