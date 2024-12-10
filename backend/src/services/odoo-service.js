d02260a256e75438ae1cfe70be47dd5b507ea6a7
remlejo <jonase2112@gmail.com>
2024-12-10 13:09:04 +0100

    extended odoo api

--- backend/src/services/odoo-service.js
+++ backend/src/services/odoo-service.js
@@ -1,4 +1,5 @@
 const Odoo = require('async-odoo-xmlrpc');
+const Salesman = require("../models/salesman/Salesman");
 
 const odoo = new Odoo({
     url: 'https://sepp-odoo.inf.h-brs.de',
@@ -10,14 +11,32 @@ const odoo = new Odoo({
 /**
  * Get all Salesman from Odoo service via XMLRPC
  *
- * @returns {Promise<*>}
+ * @returns {Salesman[]}
  * */
 exports.getAllSalesman = async () => {
     await odoo.connect();
-    return await odoo.execute_kw('hr.employee', 'search_read', [
+    const salesmen =  await odoo.execute_kw('hr.employee', 'search_read', [
         [], // Filter
-        [] // Nur diese Felder sollen zurückgegeben werden
+        ['id', 'name'] // Nur diese Felder sollen zurückgegeben werden
     ]);
+
+    return salesmen.map(salesman => {
+        const names = salesman.name.split(' ');
+        let firstName, lastName = '';
+
+        if (names.length === 2) {
+            firstName = names[0];
+            lastName = names[1];
+        } else {
+            firstName = names[0];
+        }
+
+        return Salesman.fromJSON({
+            firstname: firstName,
+            lastname: lastName,
+            sid: salesman.id
+        })
+    });
 }
 
 /**
@@ -31,10 +50,26 @@ exports.getSalesman = async (id) => {
         throw new Error('No ID given!');
 
     await odoo.connect();
-    return await odoo.execute_kw('hr.employee', 'search_read', [
+    const salesman = await odoo.execute_kw('hr.employee', 'search_read', [
         [['id', '=', id]], // Filter nach ID
-        [] // Nur diese Felder sollen zurückgegeben werden
+        ['id', 'name'] // Nur diese Felder sollen zurückgegeben werden
     ]);
+
+    const names = salesman[0].name.split(' ');
+    let firstName, lastName = '';
+
+    if (names.length === 2) {
+        firstName = names[0];
+        lastName = names[1];
+    } else {
+        firstName = names[0];
+    }
+
+    return Salesman.fromJSON({
+        firstname: firstName,
+        lastname: lastName,
+        sid: salesman[0].id
+    });
 }
 
 /**
@@ -42,11 +77,52 @@ exports.getSalesman = async (id) => {
  *
  * @returns {Promise<*>}
  * */
-exports.getBonus = async () => {
+exports.getEveryBonus = async () => {
 
     await odoo.connect();
     return await odoo.execute_kw('bonus.request', 'search_read', [
         [], // Filter
-        [] // Nur diese Felder sollen zurückgegeben werden
+        ['id', 'state', 'employee_id', 'bonus_reason_id', 'bonus_amount'] // Nur diese Felder sollen zurückgegeben werden
+    ]);
+}
+
+/**
+ * Get specific Bonus from Odoo service via XMLRPC by ID
+ *
+ * @param {number} id
+ * @returns {Promise<*>}
+ * */
+exports.getBonus = async (id) => {
+    if (!id)
+        throw new Error('No ID given!');
+
+    await odoo.connect();
+    return await odoo.execute_kw('bonus.request', 'search_read', [
+        [['id', '=', id]], // Filter nach ID
+        ['id', 'state', 'employee_id', 'bonus_reason_id', 'bonus_amount'] // Nur diese Felder sollen zurückgegeben werden
+    ]);
+}
+
+/**
+ * Get all Bonuses from Odoo service via XMLRPC for a specific Salesman
+ *
+ * @returns {Promise<*>}
+ * */
+exports.getBonusForSalesman = async (id) => {
+    if (!id)
+        throw new Error('No ID given!');
+
+    await odoo.connect();
+
+    const salesman = await this.getSalesman(id);
+
+    if (!salesman)
+        throw new Error('Salesman not found!');
+
+    const json = [salesman.id, (salesman.firstname + ' ' + salesman.lastname)];
+
+    return await odoo.execute_kw('bonus.request', 'search_read', [
+        [['employee_id', '=', json]], // Filter nach ID
+        ['id', 'state', 'employee_id', 'bonus_reason_id', 'bonus_amount'] // Nur diese Felder sollen zurückgegeben werden
     ]);
 }
