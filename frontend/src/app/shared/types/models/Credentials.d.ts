c8c5d9d33cebfce0f793b21214be4fa0b851f495
Vic Kowa <kowavic@gmail.com>
2025-01-02 16:37:33 +0100

    Created TS declaration files Credentials, Users

---
+++ frontend/src/app/shared/types/models/Credentials.d.ts
@@ -0,0 +1,11 @@
+export = Credentials;
+/**
+ * this model specifies the format to exchange credentials with the frontend
+ * @param {string} username
+ * @param {string} password
+ */
+declare class Credentials {
+    constructor(username: any, password: any);
+    username: any;
+    password: any;
+}
