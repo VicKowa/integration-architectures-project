fe91f0310366dddded454db44ae911e290109f77
Vic Kowa <kowavic@gmail.com>
2025-01-08 00:16:04 +0100

    created register functionality

--- frontend/src/app/salesman-register-page/salesman-register-page.component.ts
+++ frontend/src/app/salesman-register-page/salesman-register-page.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { NgForm} from "@angular/forms";
+import {AuthService} from "../services/auth.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {first} from "rxjs/operators";
+import UserDTO from "@app/dtos/UserDTO";
+
+
 
 @Component({
     selector: 'app-registration-view',
@@ -6,7 +13,36 @@ import { Component } from '@angular/core';
     styleUrls: ['./salesman-register-page.component.css']
 })
 export class SalesmanRegisterComponent {
-    onSubmit() {
-        console.log('Form submitted!');
+    loading = false;
+    submitted = false;
+
+    constructor(
+        private authService: AuthService,
+        private router: Router,
+        private route: ActivatedRoute
+    ) {}
+
+    onSubmit(form: NgForm) {
+        this.submitted = true;
+
+        // stop here if form is invalid
+        if (form.invalid) {
+            return;
+        }
+
+        this.loading = true;
+
+        this.authService.register(form.value as UserDTO)
+            .pipe(first())
+            .subscribe({
+                next: () => {
+                    this.router.navigate(['../login'], {relativeTo: this.route});
+                },
+                error: error => {
+                    console.error('Registration failed', error);
+                    this.loading = false;
+                }
+            });
     }
+
 }
