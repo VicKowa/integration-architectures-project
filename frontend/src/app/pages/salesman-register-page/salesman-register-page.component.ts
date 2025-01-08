375d71291c3be4b408fb74e94e33761bd59bf1e2
remlejo <jonase2112@gmail.com>
2025-01-08 17:40:07 +0100

    refactored registration, removed deprecated .group

--- frontend/src/app/pages/salesman-register-page/salesman-register-page.component.ts
+++ frontend/src/app/pages/salesman-register-page/salesman-register-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ValidatorFn, AbstractControl, ValidationErrors } from '@angular/forms';
 import { AuthService } from '@app/services/auth.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { first } from 'rxjs/operators';
@@ -12,8 +12,8 @@ import UserDTO from '@app/dtos/UserDTO';
 })
 export class SalesmanRegisterComponent implements OnInit {
     registerForm: FormGroup;
-    loading = false;
-    submitted = false;
+    loading: boolean = false;
+    submitted: boolean = false;
 
     constructor(
         private formBuilder: FormBuilder,
@@ -23,15 +23,20 @@ export class SalesmanRegisterComponent implements OnInit {
     ) {}
 
     ngOnInit(): void {
-        this.registerForm = this.formBuilder.group({
-            username: ['', Validators.required, this.validateUsername()],
+        // Initialize form with validators
+        this.registerForm = this.formBuilder.nonNullable.group({
+            username: ['', {
+                validators: Validators.required,
+                asyncValidators: [this.validateUsername()],
+                updateOn: 'blur'
+            }],
             firstname: ['', Validators.required],
             lastname: ['', Validators.required],
             email: ['', [Validators.required, Validators.email]],
             password: ['', [Validators.required]],
             confirmPassword: ['', Validators.required]
         }, {
-            validator: this.mustMatch('password', 'confirmPassword')
+            validators: this.mustMatch('password', 'confirmPassword')
         });
 
         // Check username availability on each input change
@@ -43,23 +48,34 @@ export class SalesmanRegisterComponent implements OnInit {
         }
     }
 
+    // Convenience getter for easy access to form fields
     get f() { return this.registerForm.controls; }
 
+    /**
+     * On form submission, check if form is valid and register user
+     * If registration is successful, redirect to login page
+     * */
     onSubmit(): void {
         this.submitted = true;
 
+        // Stop here if form is invalid
         if (this.registerForm.invalid) {
             return;
         }
 
         this.loading = true;
 
+        // Register user
         this.authService.register(this.registerForm.value as UserDTO)
+            //
             .pipe(first())
+            // Handle response from Server
             .subscribe({
+                // If registration is successful, redirect to login page
                 next: () => {
                     this.router.navigate(['../login'], { relativeTo: this.route });
                 },
+                // If registration fails, log error
                 error: error => {
                     console.error('Registration failed', error);
                     this.loading = false;
@@ -67,17 +83,25 @@ export class SalesmanRegisterComponent implements OnInit {
             });
     }
 
-    // Custom validator to check that two fields match
+    /**
+     * Custom validator to check if password and confirmPassword fields match
+     *
+     * @param controlName - name of the password field
+     * @param matchingControlName - name of the confirmPassword field
+     * */
     mustMatch(controlName: string, matchingControlName: string) {
-        return (formGroup: FormGroup) => {
-            const control = formGroup.controls[controlName];
-            const matchingControl = formGroup.controls[matchingControlName];
+        return (formGroup: AbstractControl): ValidationErrors | null => {
+            // Get the controls
+            const control: AbstractControl = formGroup.get(controlName);
+            const matchingControl: AbstractControl = formGroup.get(matchingControlName);
 
-            if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+            // If either control is not found, return null
+            if (matchingControl?.errors && !matchingControl.errors['mustMatch']) {
                 return;
             }
 
-            if (control.value !== matchingControl.value) {
+            // If the controls do not match, set an error on the confirmPassword field
+            if (control?.value !== matchingControl?.value) {
                 matchingControl.setErrors({ mustMatch: true });
             } else {
                 matchingControl.setErrors(null);
@@ -85,31 +109,49 @@ export class SalesmanRegisterComponent implements OnInit {
         };
     }
 
+    /**
+     * Custom async validator to check if username is already taken
+     * */
     private validateUsername(): ValidatorFn {
-        return (control: AbstractControl): {[key: string]: any} | null => {
-            if (!control.value) {
-                return null;
-            }
+        // Return a function that takes a control and returns a Promise
+        return (control: AbstractControl): Promise<ValidationErrors | null> => {
+            // Return a Promise that resolves with a ValidationErrors object
+            return new Promise((resolve) => {
+                // If the control is empty, return null
+                if (!control.value) {
+                    resolve(null);
+                    return;
+                }
 
-            this.authService.isValidUsername(control.value)
-                .subscribe(
-                    (response) => {
-                        if (!response.body.valid) {
-                            control.setErrors({"usernameTaken": true});
-                        } else {
-                            control.setErrors(null);
+                // Check if the username is already taken
+                this.authService.isValidUsername(control.value)
+                    .subscribe({
+                        // If the username is not taken, resolve with null
+                        next: (response) => {
+                            // If the response is not valid, resolve with an error object
+                            if (!response.body.valid) {
+                                resolve({ usernameTaken: false });
+                            }
+                            // If the response is valid, resolve with null
+                            else {
+                                resolve(null);
+                            }
+                        },
+                        // If an error occurs, log the error and resolve with an error object
+                        error: (error) => {
+                            console.error('Error checking username: ', error);
+                            resolve({ usernameTaken: true });
                         }
-                    },
-                    (error) => {
-                        console.log(error);
-                    }
-                );
-            return null;
+                    });
+            });
         }
-
     }
 
+    /**
+     * Check if the username is already taken
+     * */
     private checkUsername(): void {
+        // Get the username control and validate it
         const control = this.registerForm.get('username');
         if (control) {
             this.validateUsername()(control);
